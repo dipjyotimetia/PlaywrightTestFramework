49a88229b4c8191768081ec813d6746d8e1618d4
Dipjyoti Metia <dipjyotimetia@gmail.com>
2019-02-15 20:56:30 +1100

    update setup

--- setup.js
+++ setup.js
@@ -1,25 +1,25 @@
-const chalk = require('chalk')
-const puppeteer = require('puppeteer')
-const fs = require('fs')
-const mkdirp = require('mkdirp')
-const os = require('os')
-const path = require('path')
-const rimraf= require('rimraf')
+const chalk = require('chalk');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const os = require('os');
+const path = require('path');
+const rimraf = require('rimraf');
 
-const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup')
+const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 const allureReport = path.resolve(path.join(process.cwd(), `./allure-report`));
 const allureResult = path.resolve(path.join(process.cwd(), `./allure-results`));
 
-module.exports = async function () {
-    console.log(chalk.green('Setup Puppeteer'))
-    const browser = await puppeteer.launch({})
-    global.__BROWSER__ = browser
-    mkdirp.sync(DIR)
-    fs.writeFileSync(path.join(DIR, 'wsEndpoint'), browser.wsEndpoint())
-    rimraf(allureReport, () => {
-        console.log(chalk `\n{green Deleting previous allure-reports}`);
-    });
-    rimraf(allureResult, () => {
-        console.log(chalk `\n{green Deleting previous allure-results}`);
-    });
-}
+module.exports = async function() {
+  console.log(chalk.green('Setup Puppeteer'));
+  const browser = await puppeteer.launch({});
+  global.__BROWSER__ = browser;
+  mkdirp.sync(DIR);
+  fs.writeFileSync(path.join(DIR, 'wsEndpoint'), browser.wsEndpoint());
+  rimraf(allureReport, () => {
+    console.log(chalk`\n{green Deleting previous allure-reports}`);
+  });
+  rimraf(allureResult, () => {
+    console.log(chalk`\n{green Deleting previous allure-results}`);
+  });
+};
