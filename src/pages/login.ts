e272bfae4062aa5f3149e6c34e95ea18676e0b7e
Dipjyoti Metia <dipjyotimetia@gmail.com>
2021-02-25 22:45:56 +1100

    bugfix

--- src/pages/login.ts
+++ src/pages/login.ts
@@ -1,3 +1,5 @@
+const { addAttach } = require("jest-html-reporters/helper");
+
 export const LoginPge = async () => {
     try {
         await page.goto("https://react-redux.realworld.io/");
@@ -6,10 +8,13 @@ export const LoginPge = async () => {
         await page.fill('input[type="email"]', 'testauto007@gmail.com');
         await page.fill('input[type="password"]', 'Password1');
         await page.click('button[type="submit"]')
+        const screens = await page.screenshot()
+        addAttach(screens, "test screen")
         await page.waitForNavigation()
         await page.waitForTimeout(8000)
     } catch (error) {
-        await page.screenshot()
+        const screen = await page.screenshot()
+        addAttach(screen, "failed test screen")
         throw error
     }
 
