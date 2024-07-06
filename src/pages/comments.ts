e11068a10e0aa20d07dd9140daf7d2120e421ae3
Dipjyoti Metia <dipjyotimetia@gmail.com>
2024-07-06 20:46:16 +1000

    update

--- src/pages/comments.ts
+++ src/pages/comments.ts
@@ -4,16 +4,17 @@ import { Page, TestInfo } from "@playwright/test";
  * Adds comments to a webpage using Playwright's API.
  * @param page - The Playwright `Page` object representing the webpage.
  */
-export const AddComments = async (page: Page) => {
-  const commentTextarea = 'textarea[placeholder="Write a comment..."]';
-  const postCommentButton = 'text="Post Comment"';
-  const commentText = 'text="yea its working"';
-  const conduitLink = 'text="conduit"';
+export const AddComments = async (page: Page, testInfo: TestInfo) => {
+  const commentTextarea = 'textarea[placeholder="Write your article (in markdown)"]';
+  const postCommentButton = '//button[text()="Publish Article"]';
+  const conduitLink = '//a[text()="conduit"]';
 
   await page.click(commentTextarea);
   await page.fill(commentTextarea, 'yea its working');
   await page.click(postCommentButton);
-  await page.click(commentText);
-  await page.click(commentText);
   await page.click(conduitLink);
+  await testInfo.attach("AddCommentsScreenshot", {
+    body: await page.screenshot(),
+    contentType: "image/png",
+  });
 }
